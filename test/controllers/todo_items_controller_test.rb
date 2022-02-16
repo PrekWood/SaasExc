112affb190ba674269e03dc36a6b48270c1c40bc
PrekWood <nikosprek2000@gmail.com>
2022-02-16 20:08:20 +0200

    main commit

---
+++ test/controllers/todo_items_controller_test.rb
@@ -0,0 +1,23 @@
+require "test_helper"
+
+class TodoItemsControllerTest < ActionDispatch::IntegrationTest
+  # test "should get show" do
+  #   get todo_items_show_url
+  #   assert_response :success
+  # end
+  #
+  # test "should get delete" do
+  #   get todo_items_delete_url
+  #   assert_response :success
+  # end
+  #
+  # test "should get update" do
+  #   get todo_items_update_url
+  #   assert_response :success
+  # end
+  #
+  # test "should get create" do
+  #   get todo_items_create_url
+  #   assert_response :success
+  # end
+end
---
+++ test/controllers/todos_controller_test.rb
@@ -0,0 +1,38 @@
+require "test_helper"
+
+class TodosControllerTest < ActionDispatch::IntegrationTest
+  # setup do
+  #   @todo = todos(:one)
+  # end
+
+  # test "get session" do
+  #   get "/auth/get_session", as: :json
+  #   puts @response.parsed_body
+  # end
+
+  # test "should create todo" do
+  #   assert_difference("Todo.count") do
+  #     post todos_url, params: { todo: { description: @todo.description, name: @todo.name } }, as: :json
+  #   end
+  #
+  #   assert_response :created
+  # end
+  #
+  # test "should show todo" do
+  #   get todo_url(@todo), as: :json
+  #   assert_response :success
+  # end
+  #
+  # test "should update todo" do
+  #   patch todo_url(@todo), params: { todo: { description: @todo.description, name: @todo.name } }, as: :json
+  #   assert_response :success
+  # end
+  #
+  # test "should destroy todo" do
+  #   assert_difference("Todo.count", -1) do
+  #     delete todo_url(@todo), as: :json
+  #   end
+  #
+  #   assert_response :no_content
+  # end
+end
